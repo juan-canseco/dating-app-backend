a37fcf844ff9d07aa663ae6118f2a37f7982502b
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-03-06 14:25:39 -0600

    Add IdentityServer 6 Skeleton

---
+++ src/Services/Identity/Identity.API/Config.cs
@@ -0,0 +1,24 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Identity.API
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+                {
+                    new ApiScope("matching", "Matching Service")
+                };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+                { };
+    }
+}
