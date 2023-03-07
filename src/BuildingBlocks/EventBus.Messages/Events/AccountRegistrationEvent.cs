bfe1f93fb355768efc7ba4638955453fd40530ce
Juan Pablo Canseco Rios <juan.canseco.rios@gmail.com>
2023-03-07 06:45:09 -0600

    Accout Registration Event Added

---
+++ src/BuildingBlocks/EventBus.Messages/Events/AccountRegistrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using EventBus.Messages.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class AccountRegistrationEvent : BaseEvent
+    {
+        private AccountRegistrationEvent() : base(Guid.Empty, default)
+        {
+            UserId = default!;
+        }
+        public string UserId { get; private set; }
+    }
+}
